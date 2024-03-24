5ed445d3e51493eb539fb69770fda392345f23b4
Dokotela <grey.faulkenberry@mayjuun.com>
2024-03-24 10:50:30 +0200

    Updated static analysis

--- lib/enums/enums_to_string.dart
+++ lib/enums/enums_to_string.dart
@@ -11,21 +11,21 @@ import '../enums/enums.dart';
 /// Resource types, but also for some enum specifically defined for requests
 String? enumToString<T>(T enumValue) {
   switch (enumValue.runtimeType) {
-    case Compartment:
+    case Compartment _:
       return CompartmentEnumMap[enumValue];
-    case Summary:
+    case Summary _:
       return SummaryEnumMap[enumValue];
-    case dstu2.Dstu2ResourceType:
+    case dstu2.Dstu2ResourceType _:
       return dstu2.Resource.resourceTypeToString(
           enumValue as dstu2.Dstu2ResourceType);
-    case stu3.Stu3ResourceType:
+    case stu3.Stu3ResourceType _:
       return stu3.Resource.resourceTypeToString(
           enumValue as stu3.Stu3ResourceType);
-    case r4.R4ResourceType:
+    case r4.R4ResourceType _:
       return r4.Resource.resourceTypeToString(enumValue as r4.R4ResourceType);
-    case r5.R5ResourceType:
+    case r5.R5ResourceType _:
       return r5.Resource.resourceTypeToString(enumValue as r5.R5ResourceType);
-    case Mode:
+    case Mode _:
       return ModeEnumMap[enumValue];
     default:
       return enumValue.toString();
